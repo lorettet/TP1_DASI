e44e85df384da01ee4f5b456a581e30401af53d1
tlorettefr <tlorettefr@IF501-202-03.insa-lyon.fr>
2018-03-12 19:30:47 +0100

    Ajouts de données dans bddtest

--- src/main/java/service/BDDTest.java
+++ src/main/java/service/BDDTest.java
@@ -5,8 +5,12 @@
  */
 package service;
 
+import entity.Astrologue;
 import entity.Employe;
 import entity.Medium;
+import entity.Tarologue;
+import entity.Voyant;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +27,8 @@ public class BDDTest {
     
     static
     {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
+        
         employes.add(new Employe("Lorette-Froidevaux", "Théo", "lorettet", "azerty12"));
         employes.add(new Employe("Martin", "Pierre", "martinp", "azerty13"));
         employes.add(new Employe("Dupont", "lepremier", "dupontl", "azerty14"));
@@ -31,5 +37,11 @@ public class BDDTest {
         employes.add(new Employe("Leprunier", "Mathieu", "leprunierm", "azerty17"));
         employes.add(new Employe("Labrute", "Michael", "labrutem", "azerty18"));
         employes.add(new Employe("durant", "Thérèse", "durantt", "azerty19"));
+        
+        mediums.add(new Voyant("Gwenaël", "Spécialiste des grandes conversation au-delà de TOUTES frontières.", "Boule de Cristal"));
+        mediums.add(new Voyant("J.Dalmarre", "Votre avenir est devant vous: regardons-le ensemble!", "Marc de Café"));
+        mediums.add(new Tarologue("Mme Irma", "Comprenez votre entourage grâce à mes cartes! Résultats rapides.", "Tarot de Marseille"));
+        mediums.add(new Tarologue("Mme Lisa Maria NGUYINIA", "Mes cartes spécialisées pout la région Bretagne répondront à toutes vos questions personnelles.", "Tarot de Brocéliande"));
+        mediums.add(new Astrologue("Mme Sara", "Basée à Champigny-sur-Marne, Mme Sara vous révlèlera votre avenir pour éclairer vvotre passé.", "École Normale Supérieure d'Astrologie (ENS-Astro)", sdf.parse("2006")));
     }
 }
